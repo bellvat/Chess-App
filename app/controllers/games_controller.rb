116f7152dfe16df534cbb76f0338795f2995d8dc
bellvat <msirathanarun@gmail.com>
2017-10-23 05:37:08 -0700

    Updated controller- def create and game params

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,12 +9,12 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.create(game_params)
-    redirect_to game_path
+    redirect_to game_path(@game)
   end
 
   private
 
   def game_params
-    params.require(:game).permit(:white_user_id, :black_user_id)
+    params.require(:game).permit(:state,:white_user_id, :black_user_id, :winner_user_id, :turn_user_id)
   end
 end
