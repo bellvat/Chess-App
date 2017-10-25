48148b57d67d17fd81a34be68fc72291a2c544f1
bellvat <msirathanarun@gmail.com>
2017-10-25 09:50:23 -0700

    Added game#forfiet

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "games#index"
-  resources :games
+  resources :games do
+    member do
+      patch :forfeit
+      put :forfeit 
+    end
+  end
 end
