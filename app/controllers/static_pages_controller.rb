259f240ef2e74ab06e1be1d1d13f16a5c9796357
mdunnettstone <mdunnettstone@gmail.com>
2017-10-25 09:28:30 +0000

    Static page routing and controllers

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,11 @@
+class StaticPagesController < ApplicationController
+  def home
+    if current_user.present?
+      redirect_to games_path
+    end
+  end
+
+  def about
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "games#index"
+  root "static_pages#home"
+  get "/about", :controller => "static_pages", :action => "about"
   resources :games
+
+
 end
