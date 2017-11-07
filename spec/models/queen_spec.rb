33d71a26d8bb7057177ffc9e863d9c8791c2dfd2
denisekelsey <dkelsey@bellsouth.net>
2017-11-07 02:57:24 +0000

    fixed typo

--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Queen, type: :model do
   it "should return true to move three squares backward" do
       game = Game.create
       queen = FactoryGirl.create :queen, x_coord: 5, y_coord: 5, game_id: game.id
-      expect(rook.valid_move?(5, 2)).to eq(true)
+      expect(queen.valid_move?(5, 2)).to eq(true)
   end
 
   it "should return true to move two squares to the right" do
