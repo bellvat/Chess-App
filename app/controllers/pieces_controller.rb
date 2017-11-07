b10d1ca093052e027a75465f55d573a195346ca5
Tristen S <tscheitle@gmail.com>
2017-11-07 22:45:53 +0000

    Add ajax without turns

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -2,16 +2,8 @@ class PiecesController < ApplicationController
   def update
     @piece = Piece.find(params[:id])
     @game = @piece.game
-
-    if correct_turn?
-      #I think this is where we validate the move
-      # if @piece.valid_move?
-      @piece.update_attributes(piece_params)
-      redirect_to game_path(@game)
-    else
-      return render text: 'Naughty, naughty. Please wait your turn.', status: :forbidden
-    end
-
+    @piece.update_attributes(piece_params)
+    redirect_to game_path(@game)
 
   end
 
