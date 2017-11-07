d9c189dbed717b826ac10f0abdf85074bcab8333
denisekelsey <dkelsey@bellsouth.net>
2017-11-07 01:03:44 +0000

    code refactor

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,15 +1,7 @@
 class Knight < Piece
-  def valid_move?(new_x_coord, new_y_coord)
 
-    return true if new_x_coord == x_coord + 1 && new_y_coord == y_coord + 2
-    return true if new_x_coord == x_coord + 2 && new_y_coord == y_coord + 1
-    return true if new_x_coord == x_coord - 1 && new_y_coord == y_coord - 2
-    return true if new_x_coord == x_coord - 2 && new_y_coord == y_coord - 1
-    return true if new_x_coord == x_coord + 1 && new_y_coord == y_coord - 2
-    return true if new_x_coord == x_coord + 2 && new_y_coord == y_coord - 1
-    return true if new_x_coord == x_coord - 1 && new_y_coord == y_coord + 2
-    return true if new_x_coord == x_coord - 2 && new_y_coord == y_coord + 1
-  
-    return false
+  def valid_move?(new_x_coord, new_y_coord)
+    (new_x_coord == x_coord + 1 && new_y_coord == y_coord + 2) || (new_x_coord == x_coord + 2 && new_y_coord == y_coord + 1) || (new_x_coord == x_coord - 1 && new_y_coord == y_coord - 2) || (new_x_coord == x_coord - 2 && new_y_coord == y_coord - 1) || (new_x_coord == x_coord + 1 && new_y_coord == y_coord - 2) || (new_x_coord == x_coord + 2 && new_y_coord == y_coord - 1) || (new_x_coord == x_coord - 1 && new_y_coord == y_coord + 2) || (new_x_coord == x_coord - 2 && new_y_coord == y_coord + 1)
   end
+  
 end
