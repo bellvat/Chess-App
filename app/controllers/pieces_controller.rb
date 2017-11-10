e58839d7fb3933f6eb469c24d9b01401b49349ee
bellvat <msirathanarun@gmail.com>
2017-11-10 15:41:45 -0800

    Added first

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -18,7 +18,7 @@ class PiecesController < ApplicationController
   end
 
   def find_piece
-  @piece = Piece.find_by_id(params[:id])
+  @piece = Piece.find_by_id(params[:id]).first
   p @piece
   end
 
