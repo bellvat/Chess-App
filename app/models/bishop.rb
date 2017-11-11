dcb616eba575e995db8659b2ec6f20a75dc3203a
denisekelsey <dkelsey@bellsouth.net>
2017-11-11 05:21:34 +0000

    refactored code to incl diagonal method

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -4,6 +4,6 @@ class Bishop < Piece
     x_distance = x_distance(new_x_coord)
     y_distance = y_distance(new_y_coord)
 
-    (x_distance >= 1 && y_distance >= 1) && (x_distance == y_distance)
+    (x_distance >= 1 && y_distance >= 1) && diagonal?(x_distance, y_distance)
   end
 end
