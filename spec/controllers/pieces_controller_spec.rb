e9b1f7ee5ea8653c713a5291cf9567428972714e
bellvat <msirathanarun@gmail.com>
2017-11-15 14:45:33 -0800

    Added tests for piece model

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe PiecesController, type: :controller do
   it "should update coordinates if successful move" do
     current_user = FactoryGirl.create(:user, id: 1)
     sign_in current_user
-    game = Game.create turn_user_id: 1
+    game = Game.create turn_user_id: 1, white_player_user_id: 1, black_player_user_id: 2
     pawn = FactoryGirl.create :pawn, x_coord: 1, y_coord: 6, game_id: game.id, white: true
     post :update, params: {id: pawn.id, piece:{x_coord: 1, y_coord: 5}}
     expect(response).to have_http_status(200)
@@ -28,7 +28,7 @@ RSpec.describe PiecesController, type: :controller do
   it "should return success if correct player turn and move is valid" do
     current_user = FactoryGirl.create(:user, id: 1)
     sign_in current_user
-    game = Game.create turn_user_id: 1
+    game = Game.create turn_user_id: 1, white_player_user_id: 1, black_player_user_id: 2
     pawn = FactoryGirl.create :pawn, x_coord: 1, y_coord: 6, game_id: game.id, white: true
     post :update, params: {id: pawn.id, piece:{x_coord: 1, y_coord: 5}}
     expect(response).to have_http_status(200)
@@ -37,7 +37,7 @@ RSpec.describe PiecesController, type: :controller do
    it "should return error if player turn is incorrect" do
      current_user = FactoryGirl.create(:user, id: 1)
      sign_in current_user
-     game = Game.create turn_user_id: 2
+     game = Game.create turn_user_id: 2, white_player_user_id: 1, black_player_user_id: 2
      pawn = FactoryGirl.create :pawn, x_coord: 1, y_coord: 6, game_id: game.id, white: true
      post :update, params: {id: pawn.id, piece:{x_coord: 1, y_coord: 5}}
      expect(response).to have_http_status(422)
@@ -46,7 +46,7 @@ RSpec.describe PiecesController, type: :controller do
    it "should return error if invalid piece move" do
      current_user = FactoryGirl.create(:user, id: 1)
      sign_in current_user
-     game = Game.create turn_user_id: 1
+     game = Game.create turn_user_id: 1, white_player_user_id: 1, black_player_user_id: 2
      pawn = FactoryGirl.create :pawn, x_coord: 1, y_coord: 6, game_id: game.id, white: true
      post :update, params: {id: pawn.id, piece:{x_coord: 3, y_coord: 4}}
      expect(response).to have_http_status(422)
@@ -55,7 +55,7 @@ RSpec.describe PiecesController, type: :controller do
    it "should return error if the piece's move path is obstructed" do
      current_user = FactoryGirl.create(:user, id: 1)
      sign_in current_user
-     game = Game.create turn_user_id: 1
+     game = Game.create turn_user_id: 1, white_player_user_id: 1, black_player_user_id: 2
      bishop = FactoryGirl.create :bishop, x_coord: 1, y_coord: 6, game_id: game.id, white: true
      pawn = FactoryGirl.create :pawn, x_coord: 2, y_coord: 5, game_id: game.id, white: true
      post :update, params: {id: bishop.id, piece:{x_coord: 3, y_coord: 4}}
@@ -65,7 +65,7 @@ RSpec.describe PiecesController, type: :controller do
   it "should return error if the piece is moving to a square occupied by same color" do
     current_user = FactoryGirl.create(:user, id: 1)
     sign_in current_user
-    game = Game.create turn_user_id: 1
+    game = Game.create turn_user_id: 1, white_player_user_id: 1, black_player_user_id: 2
     bishop = FactoryGirl.create :bishop, x_coord: 1, y_coord: 6, game_id: game.id, white: true
     pawn = FactoryGirl.create :pawn, x_coord: 3, y_coord: 4, game_id: game.id, white: true
     post :update, params: {id: bishop.id, piece:{x_coord: 3, y_coord: 4}}
