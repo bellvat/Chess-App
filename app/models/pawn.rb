a3befd3cd0818447d6e609af8397fbb386ff32a7
bellvat <msirathanarun@gmail.com>
2017-11-16 13:12:09 -0800

    Modified comments

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -4,12 +4,12 @@ class Pawn < Piece
     x_distance = x_distance(new_x_coord)
     y_distance = y_distance(new_y_coord)
 
-# ----- Opening move -----
+# ----- Diagonal capture -----
     if !white? && opposition_piece?(new_x_coord, new_y_coord)
         (x_distance == y_distance) && (new_y_coord == (y_coord + 1))
     elsif white? && opposition_piece?(new_x_coord, new_y_coord)
         (x_distance == y_distance) && (new_y_coord == (y_coord - 1))
-# ----- Otherwise -------
+# ----- Otherwise opening and subsequent moves -------
     elsif y_coord == 2 && black?
         x_distance == 0 && (new_y_coord == 3 || new_y_coord == 4)
     elsif y_coord == 7 && white?
