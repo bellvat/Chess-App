394f724e50139e296b36d959b5ac25fe1a3eafad
denisekelsey <dkelsey@bellsouth.net>
2017-11-19 19:34:17 +0000

    WIP

--- app/models/king.rb
+++ app/models/king.rb
@@ -7,6 +7,38 @@ class King < Piece
     x_distance == 1 || y_distance == 1
   end
 
+
+# --- Castling -------
+
+# may only be done if
+# Neither the king nor the chosen rook has previously moved.
+# There are no pieces between the king and the chosen rook.
+# The king is not currently in check.
+# The king does not pass through a square that is attacked by an enemy piece (ie. King won't pass through check)
+# The king does not end up in check.
+
+  def can_castle?(new_x_coord, new_y_coord)
+    return false unless self.move_number == 0 && Rook.move_number == 0    # Neither the king nor the chosen rook has previously moved.
+    return false if is_obstructed(new_x_coord, new_y_coord)     # There are no pieces between the king and the chosen rook.
+    return false if check?    #King doesn't start, pass through, or end in check
+  end
+
+  def castle!   # Make the actual castle move
+    return false unless can_castle?
+    if queenside?
+      update_attributes(x_coord: 3, y_coord: y_coord)
+      rook.update_attributes(x_coord: 4, y_coord: y_coord)
+    else # kingside
+      update_attributes(x_coord: 7, y_coord: y_coord)
+      rook.update_attributes(x_coord: 6, y_coord: y_coord)
+    end
+  end
+
+  def queenside?
+      rook.x_coord < x_coord
+  end
+
+
 end
 
 
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -41,5 +41,63 @@ RSpec.describe King, type: :model do
 
  end
 
+# -------------- Castling -------------------
 
+  describe "#can_castle" do
+    it 'should return true if attempting to castle kingside and can castle' do
+      game = Game.create
+      king = FactoryGirl.create :king, x_coord: 5, y_coord: 1, game_id: game.id
+      rook = FactoryGirl.create :rook, x_coord: 8, y_coord: 1, game_id: game.id
+      expect(king.can_castle?(7, 1)).to eq true
+    end
+
+     it 'should return true if attempting to castle queenside and can castle' do
+      game = Game.create
+      king = FactoryGirl.create :king, x_coord: 5, y_coord: 8, game_id: game.id
+      rook = FactoryGirl.create :rook, x_coord: 1, y_coord: 8, game_id: game.id
+      expect(king.valid_move?(3, 8)).to eq true
+    end    
+  end
+
+   describe "#castle" do
+    it 'should move the correct rook to correct coord when castling kingside' do
+      game = Game.create
+      king = FactoryGirl.create :king, x_coord: 5, y_coord: 1, game_id: game.id
+      rook = FactoryGirl.create :rook, x_coord: 8, y_coord: 1, game_id: game.id
+      rook = FactoryGirl.create :rook, x_coord: 1, y_coord: 1, game_id: game.id
+      king.update_attributes(7, 0)
+      expect(game.pieces.where(x_coord: 5, y_coord: 0).first).to have_attributes(:type => "Rook")
+      expect(game.pieces.where(x_coord: 7, y_coord: 0).first).to be nil
+      expect(game.pieces.where(x_coord: 0, y_coord: 0).first).to have_attributes(:type => "Rook")
+    end
+
+    it 'should move the king to correct coord when castling kingside' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      king = FactoryGirl.create(x_coord: 4, y_coord: 0, game_id: game.id, user_id: 1 )
+      rook = FactoryGirl.create(x_coord: 0, y_coord: 0, game_id: game.id, user_id: 1)
+      rook = FactoryGirl.create(x_coord: 7, y_coord: 0, game_id: game.id, user_id: 1)
+      king.move_to!(6,0)
+      expect(king).to have_attributes(:type => "King", :x_coord => 6, :y_coord => 0 )
+    end
+
+    it 'should move the king to correct coord when castling queenside' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      king = FactoryGirl.create(x_coord: 4, y_coord: 0, game_id: game.id, user_id: 1 )
+      rook = FactoryGirl.create(x_coord: 0, y_coord: 0, game_id: game.id, user_id: 1)
+      rook = FactoryGirl.create(x_coord: 7, y_coord: 0, game_id: game.id, user_id: 1)
+      king.move_to!(2,0)
+      expect(king).to have_attributes(:type => "King", :x_coord => 2, :y_coord => 0)
+    end
+
+    it 'should move the correct rook to correct coord when castling queenside' do
+      game = Game.create(white_user_id: 0, black_user_id: 1)
+      king = FactoryGirl.create(x_coord: 4, y_coord: 0, game_id: game.id, user_id: 1 )
+      rook = FactoryGirl.create(x_coord: 0, y_coord: 0, game_id: game.id, user_id: 1 )
+      rook = FactoryGirl.create(x_coord: 7, y_coord: 0, game_id: game.id, user_id: 1)
+      king.move_to!(2,0)
+      expect(game.pieces.where(x_coord: 7, y_coord: 0).first).to have_attributes(:type => "Rook")
+      expect(game.pieces.where(x_coord: 0, y_coord: 0).first).to be nil
+      expect(game.pieces.where(x_coord: 3, y_coord: 0).first).to have_attributes(:type => "Rook")    end
+  end
 end
+
