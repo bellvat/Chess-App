5074e1bf7805f592ac719083184c832d5ff431ef
denisekelsey <dkelsey@bellsouth.net>
2017-11-27 18:37:07 +0000

    WIP

--- app/models/game.rb
+++ app/models/game.rb
@@ -87,6 +87,28 @@ class Game < ApplicationRecord
     User.find_by_id(loser_user_id)
   end
 
+  def notation
+    black = black_player_user_id
+    white = white_player_user_id
+
+
+
+    x = { 1 => "a", 2 => "b", 3 => "c", 4 => "d", 5 => "e", 6 => "f", 7 => "g", 8 => "h" }
+    y = { 1 => "8", 2 => "7", 3 => "6", 4 => "5", 5 => "4", 6 => "3", 7 => "2", 8 => "1" }
+    piece = { "King" => "K", "Queen" => "Q", "Bishop" => "B", "Knight" => "N", "Rook" => "R", "Pawn" => " " }
+
+    location = "#{piece}" + "#{x}" + "#{y}"
+
+    puts "#{location.white} / #{location.black}"
+
+   
+    get new x coord, new y_coord
+      convrt to notiation
+      puts 
+
+
+
+  end
 
 
 
