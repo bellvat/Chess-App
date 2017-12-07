7d8f298c03593a56dbfbbdfa9c294c7ae5010c1d
denisekelsey <dkelsey@bellsouth.net>
2017-12-07 05:56:16 +0000

    added username to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:username]
 
   has_many :user_games
   has_many :games, through: :user_games
   has_many :pieces
+
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  } 
+
 end
