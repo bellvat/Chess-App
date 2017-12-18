800f00739071e0887f2f8af31a44f46c98e37512
Monica Sirathanarun <msirathanarun@gmail.com>
2017-12-17 19:27:33 -0800

    Added username back in schema

--- db/schema.rb
+++ db/schema.rb
@@ -84,8 +84,10 @@ ActiveRecord::Schema.define(version: 20171217203845) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
